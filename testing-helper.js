3d434e05fd44f78a54621dc1254c995d41730457
Deepak Jayaram <deepak.jayaram@dealertrack.com>
2017-05-22 12:45:43 -0400

    Updating index files and helperjs to the latest

--- testing-helper.js
+++ testing-helper.js
@@ -1,31 +1,38 @@
 var CLIENT_VERSION_COOKIE_NAME="clientVersion";
 var CLIENT_ENV_COOKIE_NAME="clientEnvironment";
+
 var API_ENV={
     "local":"https://uds-local.dealer.com/dist/",
     "qa":"https://uds.qa.int.dealer.com/",
     "dev":"https://uds.dev.int.dealer.com/",
     "live":"https://uds.dealer.com/"
 }
+
 var client=getClientObj();
 var clientApiSrc = createClientScript(client, onLoad);
+
 function onLoad() {
     if (typeof DDC == "undefined" || typeof DDC.MyCars == "undefined" || typeof DDC.MyCars.CLIENT_API == "undefined") {
         document.getElementById('generalMessage').innerHTML = "MyCars API not loaded.  You may need to accept the certificate from <a href=\"" + clientApiSrc + "\">" + clientApiSrc + "</a>";
     } else {
+
         var div = document.createElement('div');
         html = "My cars Client Api Version: " + client.clientVersion.fontcolor("red") + "<br>";
         html += "My cars Client Api Environment: " + client.clientEnvironment.toUpperCase().fontcolor("red") + "<br>";
         html += "My cars Client API URL: " + clientApiSrc.fontcolor("red") + "<br>";
         div.innerHTML = html;
         document.body.appendChild(div);
+
         // this calls back over to the index-local html file, to separate our test harness from code that an actual client would use
         clientApiLoaded();
     }
 }
+
 function checkClientVersionCookieValue(clientVersion){
     var clientVersionReg = RegExp("^\\d+(\\.\\d+\\.\\d+)?$");
     return clientVersionReg.test(clientVersion);
 }
+
 function getClientVersion(){
     var clientVersion=getCookie(CLIENT_VERSION_COOKIE_NAME);
     if(clientVersion.length==0||!checkClientVersionCookieValue(clientVersion)){
@@ -33,37 +40,47 @@ function getClientVersion(){
     }
     return clientVersion;
 }
+
 function getClientObj(){
     var clientVersion=getClientVersion();
+
     var clientEnvCookieValue = getCookie(CLIENT_ENV_COOKIE_NAME).toLowerCase();
     var clientUrl;
+
     if (clientEnvCookieValue) {
         clientUrl = API_ENV[clientEnvCookieValue];
     }
+
     if (!clientUrl) {
         clientEnvCookieValue=DEFAULT_CLIENT_ENV;
         clientUrl=API_ENV[clientEnvCookieValue];
     }
+
     var client={
         clientVersion:clientVersion,
         clientEnvironment:clientEnvCookieValue,
         clientUrl:clientUrl
     }
+
     return client;
+
 }
+
 function createClientScript(client, callback){
     var clientScript=document.createElement("script");
     clientScript.setAttribute("id","myCarsClientScript");
     clientScript.setAttribute("src",client.clientUrl.concat("mycars-client-api-").concat(client.clientVersion).concat(".js"));
-    clientScript.setAttribute("async", true);
+    clientScript.async = true;
+    clientScript.defer = true;
     document.querySelector("head").appendChild(clientScript);
     clientScript.addEventListener("load",function(){callback()})
     clientScript.addEventListener("error",function(){callback()})
     return clientScript.src;
 }
+
 //get browser cookie's value
-function getCookie(cname) {
-    const name = cname + "=";
+function getCookie(cookieName) {
+    const name = cookieName + "=";
     const decodedCookieList = decodeURIComponent(document.cookie).split(';');
     for(let i = 0; i <decodedCookieList.length; i++) {
         const trimCookie= decodedCookieList[i].trim();
@@ -73,6 +90,9 @@ function getCookie(cname) {
     }
     return "";
 }
+
+
 function sendTabInfoToClient(tabName){
     DDC.MyCars.CLIENT_API.sendTabChange(tabName);
+
 }
